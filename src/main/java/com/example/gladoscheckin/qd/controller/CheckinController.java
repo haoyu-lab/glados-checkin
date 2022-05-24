615cf30f1a97efb7545921f1978ff68de25d4ae2
houhaoyu <15135842296@163.com>
2022-05-24 23:43:54 +0800

    请求方式修改

--- src/main/java/com/example/gladoscheckin/qd/controller/CheckinController.java
+++ src/main/java/com/example/gladoscheckin/qd/controller/CheckinController.java
@@ -4,6 +4,8 @@ import com.example.gladoscheckin.RequestTimer.CheckinTimer;
 import com.example.gladoscheckin.common.AjaxResult;
 import com.example.gladoscheckin.qd.service.CheckinService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,12 +18,12 @@ public class CheckinController {
     @Autowired
     private CheckinTimer checkinTimer;
 
-    @RequestMapping("/checkin")
+    @GetMapping("/checkin")
     public AjaxResult checkin(){
         return checkinService.checkin();
     }
 
-    @RequestMapping("/checkinTimer")
+    @PostMapping("/checkinTimer")
     public void checkinTimer(){
         checkinTimer.checkin();
     }
