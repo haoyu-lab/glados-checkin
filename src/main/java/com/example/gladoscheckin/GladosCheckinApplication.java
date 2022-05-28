d7fa11d639e2efd1d822ee8e6af6ad8728d13bb3
侯浩宇 <1411821997hhy>
2022-05-28 16:07:21 +0800

    添加数据库连接池配置druid

--- src/main/java/com/example/gladoscheckin/GladosCheckinApplication.java
+++ src/main/java/com/example/gladoscheckin/GladosCheckinApplication.java
@@ -1,8 +1,11 @@
 package com.example.gladoscheckin;
 
+import com.alibaba.druid.support.http.StatViewServlet;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
@@ -16,4 +19,14 @@ public class GladosCheckinApplication {
         SpringApplication.run(GladosCheckinApplication.class, args);
     }
 
+    @Bean
+    public ServletRegistrationBean<StatViewServlet> druidStatViewServlet() {
+        ServletRegistrationBean<StatViewServlet> registrationBean = new ServletRegistrationBean<>(new StatViewServlet(),  "/druid/*");
+        registrationBean.addInitParameter("allow", "127.0.0.1");// IP白名单 (没有配置或者为空，则允许所有访问)
+        registrationBean.addInitParameter("deny", "");// IP黑名单 (存在共同时，deny优先于allow)
+        registrationBean.addInitParameter("loginUsername", "root");
+        registrationBean.addInitParameter("loginPassword", "123456");
+        registrationBean.addInitParameter("resetEnable", "false");
+        return registrationBean;
+    }
 }
