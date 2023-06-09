cde6eb215c902b1c672ab94e471a83b0d4435648
侯浩宇 <1411821997hhy>
2023-06-09 18:00:33 +0800

    改为异步

--- src/main/java/com/example/gladoscheckin/csdnrefresh/service/impl/CsdnServiceImpl.java
+++ src/main/java/com/example/gladoscheckin/csdnrefresh/service/impl/CsdnServiceImpl.java
@@ -3,7 +3,11 @@ package com.example.gladoscheckin.csdnrefresh.service.impl;
 import com.example.gladoscheckin.csdnrefresh.service.CsdnService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.stereotype.Service;
+
+import java.util.concurrent.CompletableFuture;
 
 /**
  * @author houhaoyu
@@ -18,9 +22,14 @@ public class CsdnServiceImpl implements CsdnService {
 
     @Autowired
     private JsoupGetIp jsoupGetIp;
+    @Autowired
+    @Qualifier("asyncTaskExecutor")
+    private ThreadPoolTaskExecutor asyncTaskExecutor;
 
     @Override
     public void csdnRefresh() {
-        jsoupGetIp.csdnRefresh();
+        CompletableFuture<Void> task = CompletableFuture.runAsync(() -> {
+            jsoupGetIp.csdnRefresh();
+        },asyncTaskExecutor);
     }
 }
